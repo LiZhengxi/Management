8f37c655c0f7c16e2e19e28cf095e1c1a1e6e123
Zhengxi LI <zhengxi.li.19@neoma-bs.com>
2019-08-22 00:03:41 +0200

    Finish the database configuration part

--- ManagementForm/Ui/DatabaseUI.Designer.cs
+++ ManagementForm/Ui/DatabaseUI.Designer.cs
@@ -29,90 +29,89 @@
         private void InitializeComponent()
         {
             this.labelConnectionString = new System.Windows.Forms.Label();
-            this.textBoxConnectionString = new System.Windows.Forms.TextBox();
+            this.textBoxConnectionAdress = new System.Windows.Forms.TextBox();
             this.buttonTest = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBoxConnection = new System.Windows.Forms.TextBox();
+            this.textBoxConnectionName = new System.Windows.Forms.TextBox();
             this.buttonSave = new System.Windows.Forms.Button();
-            this.buttonConnect = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
             this.labelConnectionState = new System.Windows.Forms.Label();
+            this.textBoxConnectionSecret = new System.Windows.Forms.TextBox();
+            this.labelConnectionSecret = new System.Windows.Forms.Label();
+            this.checkBoxWindowsAuth = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // labelConnectionString
             // 
             this.labelConnectionString.AutoSize = true;
             this.labelConnectionString.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelConnectionString.Location = new System.Drawing.Point(28, 95);
+            this.labelConnectionString.Location = new System.Drawing.Point(37, 80);
+            this.labelConnectionString.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelConnectionString.Name = "labelConnectionString";
-            this.labelConnectionString.Size = new System.Drawing.Size(68, 17);
+            this.labelConnectionString.Size = new System.Drawing.Size(150, 20);
             this.labelConnectionString.TabIndex = 0;
-            this.labelConnectionString.Text = "连接字符串";
+            this.labelConnectionString.Text = "服务器地址/本机地址";
             // 
-            // textBoxConnectionString
+            // textBoxConnectionAdress
             // 
-            this.textBoxConnectionString.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxConnectionString.Location = new System.Drawing.Point(32, 122);
-            this.textBoxConnectionString.Margin = new System.Windows.Forms.Padding(10);
-            this.textBoxConnectionString.Name = "textBoxConnectionString";
-            this.textBoxConnectionString.Size = new System.Drawing.Size(341, 23);
-            this.textBoxConnectionString.TabIndex = 1;
+            this.textBoxConnectionAdress.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxConnectionAdress.Location = new System.Drawing.Point(41, 103);
+            this.textBoxConnectionAdress.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.textBoxConnectionAdress.Name = "textBoxConnectionAdress";
+            this.textBoxConnectionAdress.Size = new System.Drawing.Size(141, 27);
+            this.textBoxConnectionAdress.TabIndex = 1;
             // 
             // buttonTest
             // 
             this.buttonTest.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonTest.Location = new System.Drawing.Point(405, 125);
+            this.buttonTest.Location = new System.Drawing.Point(226, 163);
+            this.buttonTest.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonTest.Name = "buttonTest";
-            this.buttonTest.Size = new System.Drawing.Size(75, 24);
+            this.buttonTest.Size = new System.Drawing.Size(100, 40);
             this.buttonTest.TabIndex = 2;
             this.buttonTest.Text = "测试";
             this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(30, 30);
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.Size = new System.Drawing.Size(69, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "连接名字";
             // 
-            // textBoxConnection
+            // textBoxConnectionName
             // 
-            this.textBoxConnection.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxConnection.Location = new System.Drawing.Point(32, 57);
-            this.textBoxConnection.Margin = new System.Windows.Forms.Padding(10);
-            this.textBoxConnection.Name = "textBoxConnection";
-            this.textBoxConnection.Size = new System.Drawing.Size(107, 23);
-            this.textBoxConnection.TabIndex = 4;
+            this.textBoxConnectionName.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxConnectionName.Location = new System.Drawing.Point(41, 41);
+            this.textBoxConnectionName.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.textBoxConnectionName.Name = "textBoxConnectionName";
+            this.textBoxConnectionName.Size = new System.Drawing.Size(141, 27);
+            this.textBoxConnectionName.TabIndex = 4;
             // 
             // buttonSave
             // 
             this.buttonSave.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSave.Location = new System.Drawing.Point(200, 205);
+            this.buttonSave.Location = new System.Drawing.Point(41, 237);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(118, 24);
+            this.buttonSave.Size = new System.Drawing.Size(157, 35);
             this.buttonSave.TabIndex = 5;
             this.buttonSave.Text = "保存";
             this.buttonSave.UseVisualStyleBackColor = true;
-            // 
-            // buttonConnect
-            // 
-            this.buttonConnect.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonConnect.Location = new System.Drawing.Point(31, 205);
-            this.buttonConnect.Name = "buttonConnect";
-            this.buttonConnect.Size = new System.Drawing.Size(118, 24);
-            this.buttonConnect.TabIndex = 6;
-            this.buttonConnect.Text = "连接";
-            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // buttonExit
             // 
             this.buttonExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonExit.Location = new System.Drawing.Point(362, 205);
+            this.buttonExit.Location = new System.Drawing.Point(483, 237);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(118, 24);
+            this.buttonExit.Size = new System.Drawing.Size(157, 35);
             this.buttonExit.TabIndex = 7;
             this.buttonExit.Text = "退出";
             this.buttonExit.UseVisualStyleBackColor = true;
@@ -121,28 +120,64 @@
             // 
             this.labelConnectionState.AutoSize = true;
             this.labelConnectionState.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelConnectionState.Location = new System.Drawing.Point(30, 167);
+            this.labelConnectionState.Location = new System.Drawing.Point(37, 204);
+            this.labelConnectionState.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelConnectionState.Name = "labelConnectionState";
-            this.labelConnectionState.Size = new System.Drawing.Size(95, 17);
+            this.labelConnectionState.Size = new System.Drawing.Size(118, 20);
             this.labelConnectionState.TabIndex = 8;
             this.labelConnectionState.Text = "数据库连接状态:";
             // 
+            // textBoxConnectionSecret
+            // 
+            this.textBoxConnectionSecret.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxConnectionSecret.Location = new System.Drawing.Point(41, 165);
+            this.textBoxConnectionSecret.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.textBoxConnectionSecret.Name = "textBoxConnectionSecret";
+            this.textBoxConnectionSecret.Size = new System.Drawing.Size(141, 27);
+            this.textBoxConnectionSecret.TabIndex = 10;
+            // 
+            // labelConnectionSecret
+            // 
+            this.labelConnectionSecret.AutoSize = true;
+            this.labelConnectionSecret.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConnectionSecret.Location = new System.Drawing.Point(37, 142);
+            this.labelConnectionSecret.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelConnectionSecret.Name = "labelConnectionSecret";
+            this.labelConnectionSecret.Size = new System.Drawing.Size(69, 20);
+            this.labelConnectionSecret.TabIndex = 9;
+            this.labelConnectionSecret.Text = "连接密钥";
+            // 
+            // checkBoxWindowsAuth
+            // 
+            this.checkBoxWindowsAuth.AutoSize = true;
+            this.checkBoxWindowsAuth.Location = new System.Drawing.Point(225, 108);
+            this.checkBoxWindowsAuth.Name = "checkBoxWindowsAuth";
+            this.checkBoxWindowsAuth.Size = new System.Drawing.Size(145, 19);
+            this.checkBoxWindowsAuth.TabIndex = 11;
+            this.checkBoxWindowsAuth.Text = "windows身份登录";
+            this.checkBoxWindowsAuth.UseVisualStyleBackColor = true;
+            this.checkBoxWindowsAuth.CheckedChanged += new System.EventHandler(this.checkBoxWindowsAuth_CheckedChanged);
+            // 
             // DatabaseUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 246);
+            this.ClientSize = new System.Drawing.Size(660, 284);
+            this.Controls.Add(this.checkBoxWindowsAuth);
+            this.Controls.Add(this.textBoxConnectionSecret);
+            this.Controls.Add(this.labelConnectionSecret);
             this.Controls.Add(this.labelConnectionState);
             this.Controls.Add(this.buttonExit);
-            this.Controls.Add(this.buttonConnect);
             this.Controls.Add(this.buttonSave);
-            this.Controls.Add(this.textBoxConnection);
+            this.Controls.Add(this.textBoxConnectionName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonTest);
-            this.Controls.Add(this.textBoxConnectionString);
+            this.Controls.Add(this.textBoxConnectionAdress);
             this.Controls.Add(this.labelConnectionString);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "DatabaseUI";
             this.Text = "数据库管理";
+            this.Load += new System.EventHandler(this.DatabaseUI_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,13 +186,15 @@
         #endregion
 
         private System.Windows.Forms.Label labelConnectionString;
-        private System.Windows.Forms.TextBox textBoxConnectionString;
+        private System.Windows.Forms.TextBox textBoxConnectionAdress;
         private System.Windows.Forms.Button buttonTest;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBoxConnection;
+        private System.Windows.Forms.TextBox textBoxConnectionName;
         private System.Windows.Forms.Button buttonSave;
-        private System.Windows.Forms.Button buttonConnect;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.Label labelConnectionState;
+        private System.Windows.Forms.TextBox textBoxConnectionSecret;
+        private System.Windows.Forms.Label labelConnectionSecret;
+        private System.Windows.Forms.CheckBox checkBoxWindowsAuth;
     }
 }
--- ManagementForm/Ui/DatabaseUI.cs
+++ ManagementForm/Ui/DatabaseUI.cs
@@ -2,20 +2,159 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ManagementForm.Ui
 {
-    public partial class DatabaseUI : Form
+    public partial class DatabaseUI: Form
     {
+        const string databaseStatus = "数据库连接状态:";
+
         public DatabaseUI()
         {
             InitializeComponent();
         }
 
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+
+            string dataSource = textBoxConnectionAdress.Text;
+            string dataBaseKey = textBoxConnectionSecret.Text;
+            System.Data.SqlClient.SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            builder["Data Source"] = dataSource;
+           
+           
+            builder["Initial Catalog"] = "management";
+            builder["Connect Timeout"] = "5";
+            Console.WriteLine(builder.ConnectionString);
+
+            if (checkBoxWindowsAuth.Checked == true)
+            {
+                builder["integrated Security"] = true; // windows auth
+            }
+            else
+            {
+                builder["User ID"] = dataBaseKey; // password login
+                builder["Password"] = dataBaseKey;
+            }
+            testAsync(builder.ConnectionString);
+        }
+
+        public async Task testAsync (string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                   await connection.OpenAsync();
+                    labelConnectionState.Text = databaseStatus + " 成功";
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    labelConnectionState.Text = databaseStatus + " 失败";
+                }
+            }
+        }
+
+        private void checkBoxWindowsAuth_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxConnectionSecret.Enabled = checkBoxWindowsAuth.Checked ? false : true;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // Get the application configuration file.
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            // Construct a sql connection string builder 
+            System.Data.SqlClient.SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            
+            // Check if all the textbox is filled 
+            if (textBoxConnectionAdress.Text != "" && textBoxConnectionName.Text != "")
+            {
+                builder["Data Source"] = textBoxConnectionAdress.Text;
+                builder["Initial Catalog"] = "management";
+                // check box
+                if (checkBoxWindowsAuth.Checked)
+                {
+                    builder["integrated Security"] = true; // windows auth
+                }
+                // sql connection mode 
+                else
+                {
+                    if(textBoxConnectionSecret.Text !="")
+                    {
+                        builder["User ID"] = textBoxConnectionSecret.Text; // password login
+                        builder["Password"] = textBoxConnectionSecret.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("请正确填写数据库资料");
+                    }
+                }
+                // Create a connection string element.
+                ConnectionStringSettings csSettings = new ConnectionStringSettings("ManagementDbContext", builder.ConnectionString, "System.Data.SqlClient");
+                //// Get the connection strings section.
+                ConnectionStringsSection csSection = config.ConnectionStrings;
+                // Add the new element.
+                csSection.ConnectionStrings.Clear();
+                csSection.ConnectionStrings.Add(csSettings);
+                // Save the configuration file.
+                config.Save(ConfigurationSaveMode.Modified);
+                MessageBox.Show("保存成功");
+            }
+            else
+            {
+                MessageBox.Show("请正确填写数据库资料");
+            }
+        }
+
+        private void DatabaseUI_Load(object sender, EventArgs e)
+        {
+            string connectionName = "";
+            string connectionString = "";
+            string connectionProvider = "System.Data.SqlClient";
+
+            // Get the application configuration file.
+            
+            System.Configuration.Configuration config =
+                    ConfigurationManager.OpenExeConfiguration(
+                    ConfigurationUserLevel.PerUserRoamingAndLocal);
+
+            // Get the conectionStrings section.
+            ConnectionStringsSection csSection =
+                config.ConnectionStrings;
+            for (int i = 0; i < ConfigurationManager.ConnectionStrings.Count; i++)
+            {
+                ConnectionStringSettings cs = csSection.ConnectionStrings[i];
+                connectionName = cs.Name;
+                if (connectionName == "ManagementDbContext" && cs.ProviderName == connectionProvider)
+                {
+                    connectionString = cs.ConnectionString;
+                    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+                    // check the windows login 
+                    if (builder.IntegratedSecurity)
+                    {
+                        checkBoxWindowsAuth.Checked = true;
+                        textBoxConnectionSecret.Enabled = false;
+                    }
+                    // Datasource 
+                    textBoxConnectionAdress.Text = builder.DataSource;
+                    // Check if the windows login is no true
+                    textBoxConnectionSecret.Text = builder.UserID;
+                    break;
+                }
+            }
+            textBoxConnectionName.Text = connectionName;
+        }
     }
 }
